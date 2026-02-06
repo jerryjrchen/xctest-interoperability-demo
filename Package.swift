439524463988f335b7af1b0ad698f0ac0d180677
Jerry Chen <jerryjrchen@apple.com>
2026-02-05 17:20:00 -0800

    Add license

--- Package.swift
+++ Package.swift
@@ -1,6 +1,16 @@
 // swift-tools-version: 6.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2026 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+//
+
 import PackageDescription
 
 let package = Package(
